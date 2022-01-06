d4f638c6209d5bc2478fd9c07e12ea2281418a98
Kavish13 <91259166+Kavish13@users.noreply.github.com>
2022-01-06 15:33:55 +0530

    1st project

---
+++ intro.java
@@ -0,0 +1,243 @@
+import java.util.*;
+class intro
+{	
+	void harshad()
+	{
+	String name1="Harshad Pachpinde";
+	String city1="Kalyan";
+	String edu1="Completed Computer Engineering in Current year(2021)";
+	String exp1="No Experience";
+		System.out.println("Name:       "+name1+"\nCity:       "+city1+"\nEducation:  "+edu1+"\nExperience: "+exp1);
+		System.out.println("\n\n\nPress 1 to go BACK");
+		Scanner h1=new Scanner(System.in);
+		int q1=h1.nextInt();
+		if(q1==1)
+		{
+			main(null);
+		}
+		else harshad();
+		
+		
+		
+		
+	}
+	void kavish()
+	{
+		String name2="Kavish Dahake";
+		String city2="Nagpur";
+		String edu2="Completed Power Engineering in ";
+		String exp2="6 Month of Work Experience";
+		System.out.println("Name:       "+name2+"\nCity:       "+city2+"\nEducation:  "+edu2+"\nExperience: "+exp2);
+		System.out.println("\n\n\nPress 1 to go BACK");
+		Scanner h1=new Scanner(System.in);
+		int q1=h1.nextInt();
+		if(q1==1)
+		{
+			main(null);
+		}
+		else kavish();
+	}
+	void lov()
+	{
+		String name3="Lov Tiwari";
+		String city3="Kolhapur";
+		String edu3="Completed E&TC Engineering in Current year(2021)";
+		String exp3="No Experience";
+		System.out.println("Name:       "+name3+"\nCity:       "+city3+"\nEducation:  "+edu3+"\nExperience: "+exp3);
+		System.out.println("\n\n\nPress 1 to go BACK");
+		Scanner h1=new Scanner(System.in);
+		int q1=h1.nextInt();
+		if(q1==1)
+		{
+			main(null);
+		}
+		else lov();
+	}
+	void himanshi()
+	{
+		String name4="Himanshi Bante";
+		String city4="Nagpur";
+		String edu4="Completed Graduation in Computer Science in 2019)";
+		String exp4="1.5 yrs of Experience in IBM ";
+		System.out.println("Name:       "+name4+"\nCity:       "+city4+"\nEducation:  "+edu4+"\nExperience: "+exp4);
+		System.out.println("\n\n\nPress 1 to go BACK");
+		Scanner h1=new Scanner(System.in);
+		int q1=h1.nextInt();
+		if(q1==1)
+		{
+			main(null);
+		}
+		else himanshi();
+	}
+	void mayurdalvi()
+	{
+		String name5="Mayur Dalvi";
+		String city5="Mumbai";
+		String edu5="Completed Computer Engineering in 2017 ";
+		String exp5="2 yrs of experience in Netmagic as a Windows Administrator";
+		System.out.println("Name:       "+name5+"\nCity:       "+city5+"\nEducation:  "+edu5+"\nExperience: "+exp5);
+		System.out.println("\n\n\nPress 1 to go BACK");
+		Scanner h1=new Scanner(System.in);
+		int q1=h1.nextInt();
+		if(q1==1)
+		{
+			main(null);
+		}
+		else mayurdalvi();
+	}
+	void kamran()
+	{
+		String name6="Kamran Shaikh";
+		String city6="Kalyan";
+		String edu6="Completed Graduation in Instrumentation in 2017";
+		String exp6="3 yrs of work Experience";
+		System.out.println("Name:       "+name6+"\nCity:       "+city6+"\nEducation:  "+edu6+"\nExperience: "+exp6);
+		System.out.println("\n\n\nPress 1 to go BACK");
+		Scanner h1=new Scanner(System.in);
+		int q1=h1.nextInt();
+		if(q1==1)
+		{
+			main(null);
+		}
+		else kamran();
+	}
+	void mayurdighe()
+	{
+		String name7="Mayur Dighe";
+		String city7="Ahmednagar";
+		String edu7="Completed Mechanical Engineering in 2019";
+		String exp7="No Experience";
+		System.out.println("Name:       "+name7+"\nCity:       "+city7+"\nEducation:  "+edu7+"\nExperience: "+exp7);
+		System.out.println("\n\n\nPress 1 to go BACK");
+		Scanner h1=new Scanner(System.in);
+		int q1=h1.nextInt();
+		if(q1==1)
+		{
+			main(null);
+		}
+		else mayurdighe();
+	}
+	void mandar()
+	{
+		String name8="Mandar Sadgir";
+		String city8="Kalyan";
+		String edu8="Completed Electronics Engineering in 2019";
+		String exp8="1 yr of Experience as Service Engineer";
+		System.out.println("Name:       "+name8+"\nCity:       "+city8+"\nEducation:  "+edu8+"\nExperience: "+exp8);
+		System.out.println("\n\n\nPress 1 to go BACK");
+		Scanner h1=new Scanner(System.in);
+		int q1=h1.nextInt();
+		if(q1==1)
+		{
+			main(null);
+		}
+		else mandar();
+	}
+	void mayurchitte()
+	{
+		String name9="Mayur Chitte ";
+		String city9="Shirpur";
+        String edu9="Completed Mechanical Engineering in 2020";
+		String exp9="No Experience";
+		System.out.println("Name:       "+name9+"\nCity:       "+city9+"\nEducation:  "+edu9+"\nExperience: "+exp9);
+		System.out.println("\n\n\nPress 1 to go BACK");
+		Scanner h1=new Scanner(System.in);
+		int q1=h1.nextInt();
+		if(q1==1)
+		{
+			main(null);
+		}
+		else mayurchitte();
+	}
+	void jayesh()
+	{
+		String name10="Jayesh Bari";
+		String city10="Shirpur";
+		String edu10="Completed E&TC Engineering in 2020";
+		String exp10="No Experience";
+		System.out.println("Name:       "+name10+"\nCity:       "+city10+"\nEducation:  "+edu10+"\nExperience: "+exp10);
+		System.out.println("\n\n\nPress 1 to go BACK");
+		Scanner h1=new Scanner(System.in);
+		int q1=h1.nextInt();
+		if(q1==1)
+		{
+			main(null);
+		}
+		else jayesh();
+	}
+	public static void main(String args[])
+	{	System.out.println("      *       * * * * *  * * * * *  * * * * *  * * * * *        *       *      *  * * * * * *  * * * * *");
+	    System.out.println("     * *      *          *       *      *      *       *       * *      * *    *      *        *        ");
+	    System.out.println("    *   *     *          *       *      *      *       *      *   *     *  *   *      *        *  	    ");
+        System.out.println("   * * * *    * * * * *  * * * * *      *      * * * * *     * * * *    *   *  *      *        * * * * *");     
+        System.out.println("  *       *           *  *              *      *  *         *       *   *    * *      *                *");
+        System.out.println(" *         *          *  *              *      *    *      *         *  *     **      *                *");
+        System.out.println("*           * * * * * *  *          * * * * *  *       *  *           * *      *      *        * * * * *");
+		Scanner s= new Scanner(System.in);
+		
+		System.out.println("1.HIMANSHI BANTE    - COMMUNICATION LEAD\n2.MAYUR DALVI       - TECHNICAL LEAD\n3.MAYUR DIGHE       - APTITUDE LEAD\n4.HARSHAD PACHPINDE\n5.KAVISH DAHAKE\n6.LOV TIWARI\n7.KAMRAN SHAIKH\n8.MANDAR SADGIR\n9.MAYUR CHITTE\n10.JAYESH BARI\n11.exit from program ");
+		System.out.println("ENTER THE NUMBER CODE OF STUDENT");
+		int a=s.nextInt();
+		intro i1=new intro();
+		switch(a)
+		{
+			case 1:
+			{
+				i1.himanshi();
+				break;
+			}
+			case 2:
+			{
+				i1.mayurdalvi();
+				break;
+			}
+			case 3:
+			{
+				i1.mayurdighe();
+				break;
+			}
+			case 4:
+			{
+				i1.harshad();
+				break;
+			}
+			case 5:
+			{
+				i1.kavish();
+				break;
+			}
+			case 6:
+			{
+				i1.lov();
+				break;
+			}
+			case 7:
+			{
+				i1.kamran();
+				break;
+			}
+			case 8:
+			{
+				i1.mandar();
+				break;
+			}
+			case 9:
+			{
+				i1.mayurchitte();
+				break;
+			}
+			case 10:
+			{
+				i1.jayesh();
+				break;
+			}
+			case 11:
+			{
+				
+				break;
+			}
+			default: {System.out.println("ENTER THE NUMBER BETWEEN 1 TO 10");main(null);}
+		}
+	}
+	
+}
